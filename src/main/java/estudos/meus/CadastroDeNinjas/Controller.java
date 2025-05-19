532a515a18b2d58e905f902f56bad8e15f8a0d7c
Eduardo-Desplanches <eduardodasantos34@gmail.com>
2025-05-19 12:47:41 -0300

    Controller creation

---
+++ src/main/java/estudos/meus/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package estudos.meus.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    /*Devem se minúsculo as rotas*/
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+}
