9641625aa64cdeb1989f3d81fa04c5ef6c71ff19
Eduardo-Desplanches <eduardodasantos34@gmail.com>
2025-05-28 00:13:51 -0300

    feat: Cria as rotas do controller com Mock

--- src/main/java/estudos/meus/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/estudos/meus/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,40 @@
 package estudos.meus.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+// Criar rota para o servidor
+//Controller perto do usuário
+//Controller faz a requisição pelo usuário
+
+@RestController //Diz que tudo será um controller
+@RequestMapping ("missoes")//Mapea a API
 public class MissoesController {
+
+    //Post -> Manda uma requisição para criar as missões
+    @PostMapping("/adicionarmissoes")
+    public String criarMissoes(){
+        return "Missão Adicionada";
+    }
+    @GetMapping("/todas")
+    public String mostrarTodasMissoes(){
+        return "Mostrando todas as missões";
+    }
+
+    //Get -> Manda uma requisição para mostrar as missões
+    @GetMapping("/todasid")
+    public String mostrarMissoesPorId(){
+        return "Mostrando todas missão por Id";
+    }
+
+    //Put -> Manda uma requisição para alterar as missões
+    @PutMapping("/alterarid")
+    public String alterarMissoes(){
+        return "Alterando missões por Id";
+    }
+
+    //Delete -> Manda uma requisição para deletar as missões
+    @DeleteMapping("/deletarid")
+    public String deletarMissoes(){
+        return "Deletando missões por Id";
+    }
 }
